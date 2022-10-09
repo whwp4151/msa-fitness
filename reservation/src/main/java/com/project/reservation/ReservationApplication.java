a9ad156300e1877639db2a25a9ebafa049640cfc
whwp4151 <whwp4151@naver.com>
2022-10-09 18:10:24 +0900

    reservation : 프로젝트 세팅

--- reservation/src/main/java/com/project/reservation/ReservationApplication.java
+++ reservation/src/main/java/com/project/reservation/ReservationApplication.java
@@ -2,8 +2,12 @@ package com.project.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableJpaAuditing
 public class ReservationApplication {
 
 	public static void main(String[] args) {
---
+++ reservation/src/main/java/com/project/reservation/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.project.reservation.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server= new Server();
+        server.setUrl("http://localhost:8072");
+
+        //Bearer token 설정
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement schemaRequirement = new SecurityRequirement().addList("bearerAuth");
+
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .addSecurityItem(schemaRequirement)
+                .security(Arrays.asList(schemaRequirement))
+                .servers(List.of(server))
+                .info(new Info().title("reservation Service API")
+                        .description("MSA - Reservation Service입니다.")
+                        .version("v0.0.1"));
+    }
+}
